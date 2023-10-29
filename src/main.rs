eb672b097ad3fc0d6175bb4eaeaf09d1bca2127a
Thierry Berger <contact@thierryberger.com>
2023-10-29 15:07:23 +0100

    tried to add frame debugging capability but vsync is failing (running at 400+ frames per second)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-use bevy::{a11y::AccessibilityPlugin, input::InputPlugin, prelude::*, winit::WinitPlugin};
+use bevy::{
+    a11y::AccessibilityPlugin,
+    app::ScheduleRunnerPlugin,
+    core_pipeline::CorePipelinePlugin,
+    input::InputPlugin,
+    log::LogPlugin,
+    pbr::PbrPlugin,
+    prelude::*,
+    render::{render_asset::RenderAssetPlugin, RenderPlugin},
+    time::TimePlugin,
+    winit::WinitPlugin,
+};
 
 fn main() {
     App::new()
@@ -7,6 +18,17 @@ fn main() {
             WinitPlugin,
             InputPlugin,
             AccessibilityPlugin,
+            // For tracy
+            TimePlugin::default(),
+            TaskPoolPlugin::default(),
+            AssetPlugin::default(),
+            RenderPlugin::default(),
+            ImagePlugin::default(),
+            LogPlugin::default(),
+            ScheduleRunnerPlugin::default(),
+            CorePipelinePlugin::default(),
+            FrameCountPlugin::default(),
+            PbrPlugin::default(),
         ))
         .run();
 }
